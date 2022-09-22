b514ec8ff461457e91fdd3b5fba9a0e73a06d4be
edoardopaolinelli <edoardo.paolinelli@gmail.com>
2022-09-22 15:17:08 +0200

    Aggiunti i controlli ai vari campi di autenticazione

--- lib/widgets/authentication_form.dart
+++ lib/widgets/authentication_form.dart
@@ -8,6 +8,24 @@ class AuthenticationForm extends StatefulWidget {
 }
 
 class _AuthenticationFormState extends State<AuthenticationForm> {
+  final _formKey = GlobalKey<FormState>();
+  String? _userEmail = '';
+  String? _userName = '';
+  String? _userPassword = '';
+  bool _isLogin = true;
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+    if (isValid) {
+      _formKey.currentState!.save();
+      FocusScope.of(context).unfocus();
+      print(_userEmail);
+      print(_userName);
+      print(_userPassword);
+      //Devo inviare la richiesta a firebase
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -17,10 +35,25 @@ class _AuthenticationFormState extends State<AuthenticationForm> {
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    key: const ValueKey('email'),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter a email address';
+                      } else if (!RegExp(
+                              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value)) {
+                        return 'Please enter a valid email address';
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      _userEmail = newValue;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: const InputDecoration(
                       labelText: 'Email address',
@@ -29,15 +62,39 @@ class _AuthenticationFormState extends State<AuthenticationForm> {
                       ),
                     ),
                   ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Username',
-                      labelStyle: TextStyle(
-                        fontFamily: 'Lato-Bold',
+                  if (!_isLogin)
+                    TextFormField(
+                      key: const ValueKey('username'),
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 4) {
+                          return 'Please enter at least 4 characters';
+                        }
+                      },
+                      onSaved: (newValue) {
+                        _userName = newValue;
+                      },
+                      decoration: const InputDecoration(
+                        labelText: 'Username',
+                        labelStyle: TextStyle(
+                          fontFamily: 'Lato-Bold',
+                        ),
                       ),
                     ),
-                  ),
                   TextFormField(
+                    key: const ValueKey('password'),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter a password';
+                      } else if (!RegExp(
+                              r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$')
+                          .hasMatch(value)) {
+                        return 'Please enter a valid password';
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      _userPassword = newValue;
+                    },
                     decoration: const InputDecoration(
                       labelText: 'Password',
                     ),
@@ -45,10 +102,8 @@ class _AuthenticationFormState extends State<AuthenticationForm> {
                   ),
                   const SizedBox(height: 12),
                   ElevatedButton(
-                    onPressed: () {
-                      //
-                    },
-                    child: const Text('Login'),
+                    onPressed: _trySubmit,
+                    child: Text(_isLogin ? 'Login' : 'SignUp'),
                   ),
                   TextButton(
                     style: const ButtonStyle(
@@ -56,9 +111,13 @@ class _AuthenticationFormState extends State<AuthenticationForm> {
                           MaterialStatePropertyAll<Color>(Colors.indigo),
                     ),
                     onPressed: () {
-                      //
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
                     },
-                    child: const Text('Create new account'),
+                    child: Text(_isLogin
+                        ? 'Create new account'
+                        : 'I already have an account'),
                   ),
                 ],
               ),
